262110f90eed9f66a9cf0aeccb3ca6e3b732fc33
trmdn <doludunyao@gmail.com>
2024-03-03 22:57:28 +0200

    first_steps_in_oop_exercises

--- first_steps_in_oop/01.rhombus_of_stars.py
+++ first_steps_in_oop/01.rhombus_of_stars.py
@@ -1,8 +1,25 @@
-n = int(input())
+number_of_rhombus = int(input())
 
-def rhombus_top(size):
-    for row in range(1, size + 1):
-        pass
+class Rhombus():
+    def __init__(self, stars):
+        self.stars = stars
+        self.draw = []
+    
+    def make_rhombus(self):
+        for row in range(1, self.stars + 1):
+            if row == 1:
+                self.draw.append((self.stars - row) * " " + row * "*")
+            else:
+                self.draw.append((self.stars - row) * " " + row * "* ")
+        for rows in range(0, self.stars - 1):
+            if rows == 0:
+                self.draw.append((self.stars - (row - 1)) * " " + (self.stars - 1) * "* ")
+            else:
+                self.draw.append((rows + 1) * " " + (self.stars - rows - 1) * "* ")
+    
+    def __repr__(self):
+        return "\n".join(self.draw)
 
-def rhombus():
-    pass
+r = Rhombus(number_of_rhombus)
+r.make_rhombus()
+print(r)
---
+++ first_steps_in_oop_exercise/project/trainer.py
@@ -0,0 +1,34 @@
+from typing import List
+from project.pokemon import Pokemon
+
+class Trainer():
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemons: List[Pokemon] = []
+
+    def add_pokemon(self, pokemon: Pokemon) -> str:
+        if pokemon in self.pokemons:
+            return "This pokemon is already caught"
+        
+        self.pokemons.append(pokemon)
+
+        return f"Caught {pokemon.pokemon_details()}"
+    
+    def release_pokemon(self, pokemon_name: str):
+        pokemon = None
+
+        for p in self.pokemons:
+            if p.name == pokemon_name:
+                pokemon = p
+                break
+        
+        self.pokemons.remove(pokemon)
+
+        return f"You have released {pokemon_name}"
+    
+    def trainer_data(self) -> str:
+        pokemons_data = "\n".join(f"- {p.pokemon_details()}" for p in self.pokemons)
+
+        return f"Pokemon Trainer {self.name}\n" \
+               f"Pokemon count {len(self.pokemons)}\n" \
+               f"{pokemons_data}"
