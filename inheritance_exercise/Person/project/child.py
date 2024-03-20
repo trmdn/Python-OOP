4565314fdd3736637018829f56b3cd2791076f8a
trmdn <doludunyao@gmail.com>
2024-03-20 16:06:04 +0200

    inheritance_exercises

--- inheritance_exercise/Person/project/child.py
+++ inheritance_exercise/Person/project/child.py
@@ -9,5 +9,4 @@ person = Person("Peter", 25)
 child = Child("Peter Junior", 5)
 print(person.name)
 print(person.age)
-
 print(child.__class__.__bases__[0].__name__)
---
+++ inheritance_exercise/Zoo/project/animal.py
@@ -0,0 +1,12 @@
+class Animal:
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+# mammal = Mammal("Stella")
+# print(mammal.__class__.__bases__[0].__name__)
+# print(mammal.name)
+# lizard = Lizard("John")
+# print(lizard.__class__.__bases__[0].__name__)
+# print(lizard.name)
